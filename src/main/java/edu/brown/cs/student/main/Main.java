b521bdaec61dc743f4b7c9f718c59f3ab95bc788
SirGnat <eric_tang@brown.edu>
2022-03-03 20:20:57 -0500

    finish

--- src/main/java/edu/brown/cs/student/main/Main.java
+++ src/main/java/edu/brown/cs/student/main/Main.java
@@ -6,118 +6,125 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import freemarker.template.Configuration;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+import org.json.JSONException;
+import org.json.JSONObject;
 import spark.*;
 import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  * The Main class of our project. This is where execution begins.
- *
  */
 public final class Main {
 
-  private static final int DEFAULT_PORT = 4567;
+    private static final int DEFAULT_PORT = 4567;
 
-  /**
-   * The initial method called when execution begins.
-   *
-   * @param args
-   *             An array of command line arguments
-   */
-  public static void main(String[] args) {
-    new Main(args).run();
-  }
-
-  private String[] args;
-
-  private Main(String[] args) {
-    this.args = args;
-  }
-
-  private void run() {
-    OptionParser parser = new OptionParser();
-    parser.accepts("gui");
-    parser.accepts("port").withRequiredArg().ofType(Integer.class)
-        .defaultsTo(DEFAULT_PORT);
-
-    OptionSet options = parser.parse(args);
-    if (options.has("gui")) {
-      runSparkServer((int) options.valueOf("port"));
+    /**
+     * The initial method called when execution begins.
+     *
+     * @param args An array of command line arguments
+     */
+    public static void main(String[] args) {
+        new Main(args).run();
     }
-  }
 
-  private void runSparkServer(int port) {
-    Spark.port(port);
-    Spark.exception(Exception.class, new ExceptionPrinter());
+    private String[] args;
 
-    // Setup Spark Routes
-
-    // TODO: create a call to Spark.post to make a POST request to a URL which
-    // will handle getting matchmaking results for the input
-    // It should only take in the route and a new ResultsHandler
-    Spark.options("/*", (request, response) -> {
-      String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-      if (accessControlRequestHeaders != null) {
-        response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-      }
-
-      String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-
-      if (accessControlRequestMethod != null) {
-        response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-      }
-
-      return "OK";
-    });
-
-    // Allows requests from any domain (i.e., any URL). This makes development
-    // easier, but it’s not a good idea for deployment.
-    Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
-  }
-
-  /**
-   * Display an error page when an exception occurs in the server.
-   */
-  private static class ExceptionPrinter implements ExceptionHandler {
-    @Override
-    public void handle(Exception e, Request req, Response res) {
-      res.status(500);
-      StringWriter stacktrace = new StringWriter();
-      try (PrintWriter pw = new PrintWriter(stacktrace)) {
-        pw.println("<pre>");
-        e.printStackTrace(pw);
-        pw.println("</pre>");
-      }
-      res.body(stacktrace.toString());
+    private Main(String[] args) {
+        this.args = args;
     }
-  }
 
-  /**
-   * Handles requests for horoscope matching on an input
-   * 
-   * @return GSON which contains the result of MatchMaker.makeMatches
-   */
-  private static class ResultsHandler implements Route {
-    @Override
-    public String handle(Request req, Response res) {
-      // TODO: Get JSONObject from req and use it to get the value of the sun, moon,
-      // and rising
-      // for generating matches
+    private void run() {
+        OptionParser parser = new OptionParser();
+        parser.accepts("gui");
+        parser.accepts("port").withRequiredArg().ofType(Integer.class)
+                .defaultsTo(DEFAULT_PORT);
 
-      // TODO: use the MatchMaker.makeMatches method to get matches
-
-      // TODO: create an immutable map using the matches
-
-      // TODO: return a json of the suggestions (HINT: use GSON.toJson())
-      Gson GSON = new Gson();
-      return null;
+        OptionSet options = parser.parse(args);
+        if (options.has("gui")) {
+            runSparkServer((int) options.valueOf("port"));
+        }
     }
-  }
+
+    private void runSparkServer(int port) {
+        Spark.port(port);
+        Spark.exception(Exception.class, new ExceptionPrinter());
+
+        // Setup Spark Routes
+
+        // TODO: create a call to Spark.post to make a POST request to a URL which
+        // will handle getting matchmaking results for the input
+        // It should only take in the route and a new ResultsHandler
+        Spark.post("/results", new ResultsHandler());
+
+        Spark.options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        // Allows requests from any domain (i.e., any URL). This makes development
+        // easier, but it’s not a good idea for deployment.
+        Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+    }
+
+    /**
+     * Display an error page when an exception occurs in the server.
+     */
+    private static class ExceptionPrinter implements ExceptionHandler {
+        @Override
+        public void handle(Exception e, Request req, Response res) {
+            res.status(500);
+            StringWriter stacktrace = new StringWriter();
+            try (PrintWriter pw = new PrintWriter(stacktrace)) {
+                pw.println("<pre>");
+                e.printStackTrace(pw);
+                pw.println("</pre>");
+            }
+            res.body(stacktrace.toString());
+        }
+    }
+
+    /**
+     * Handles requests for horoscope matching on an input
+     *
+     * @return GSON which contains the result of MatchMaker.makeMatches
+     */
+    private static class ResultsHandler implements Route {
+        @Override
+        public String handle(Request req, Response res) throws JSONException {
+            // TODO: Get JSONObject from req and use it to get the value of the sun, moon, and rising for generating matches
+
+            JSONObject jsonObject = new JSONObject(req.body());
+            String sun = jsonObject.getString("sun");
+            String moon = jsonObject.getString("moon");
+            String rising = jsonObject.getString("rising");
+            // TODO: use the MatchMaker.makeMatches method to get matches
+            List<String> matches = MatchMaker.makeMatches(sun, moon, rising);
+            // TODO: create an immutable map using the matches
+            Map<String, Object> matchMap = ImmutableMap.of("matches", matches);
+            // TODO: return a json of the suggestions (HINT: use GSON.toJson())
+            Gson GSON = new Gson();
+            return GSON.toJson(matchMap);
+        }
+    }
 }
--- src/main/site/ts/main.ts
+++ src/main/site/ts/main.ts
@@ -1,23 +1,41 @@
 // TODO: select the list element where the suggestions should go, and all three dropdown elements
 //  HINT: look at the HTML
-
+const suggestions = document.querySelector('#suggestions')
+const sun = document.querySelector('#sun') as HTMLInputElement
+const rising = document.querySelector('#moon') as HTMLInputElement
+const moon = document.querySelector('#rising') as HTMLInputElement
 // Here, when the value of sun is changed, we will call the method postAndUpdate.
+// @ts-ignore
+sun.addEventListener("change", postAndUpdate)
 // TODO: Do the same for moon and rising
-
+// @ts-ignore
+moon.addEventListener("change", postAndUpdate)
+// @ts-ignore
+rising.addEventListener("change", postAndUpdate)
 // TODO: Define a type for the request data object here.
-// type MatchesRequestData = {}
+type MatchesRequestData = {
+  sun : string,
+  moon : string,
+  rising : string
+}
 
 // TODO: Define a type for the response data object here.
-// type Matches = {}
+type Matches = {
+  matches : string[]
+}
 
 function postAndUpdate(): void {
   // TODO: empty the suggestionList (you want new suggestions each time someone types something new)
   //  HINT: use .innerHTML
-
+  // @ts-ignore
+  suggestions.innerHTML = ''
   // TODO: add a type annotation to make this of type MatchesRequestData
-  const postParameters = {
+  const postParameters : MatchesRequestData = {
     // TODO: get the text inside the input box
     //  HINT: use sun.value to get the value of the sun field, for example
+    sun : sun.value,
+    moon : moon.value,
+    rising : rising.value
   };
 
   console.log(postParameters)
@@ -26,7 +44,13 @@ function postAndUpdate(): void {
   //  HINT: check out the POST REQUESTS section of the lab and of the front-end guide.
   //  Make sure you add "Access-Control-Allow-Origin":"*" to your headers.
   //  Remember to add a type annotation for the response data using the Matches type you defined above!
-
+  const result = fetch("http://localhost:4567/results", {
+    method: 'POST',
+    body: JSON.stringify(postParameters),
+    headers: {
+      "Access-Control-Allow-Origin":"*"
+    }
+  }).then((response) => response.json()).then(x => updateSuggestions(x.matches))
   // TODO: Call and fill in the updateSuggestions method in one of the .then statements in the Promise
   //  Parse the JSON in the response object
   //  HINT: remember to get the specific field in the JSON you want to use
@@ -38,6 +62,11 @@ function updateSuggestions(matches: string[]): void {
   //  NOTE: you should use <li> (list item) tags to wrap each element. When you do so,
   //  make sure to add the attribute 'tabindex="0"' (for example: <li tabindex="0">{your element}</li>).
   //  This makes each element selectable via screen reader.
+  for (let match of matches) {
+    // @ts-ignore
+    suggestions.innerHTML += `<li tabindex="0">${match}</li>`
+  }
+
 }
 
 // TODO: create an event listener to the document (document.addEventListener) that detects "keyup".
@@ -45,13 +74,17 @@ function updateSuggestions(matches: string[]): void {
 //  values for the sun, moon, and rising using updateValues. Then call postAndUpdate().
 //  HINT: the listener callback function should be asynchronous and wait until the values are
 //  updated before calling postAndUpdate().
+document.addEventListener("keyup", async function test () { await updateValues("Gemini", "Gemini", "Gemini") })
+postAndUpdate();
 
 async function updateValues(sunval: string, moonval: string, risingval: string): Promise<void>{
   // This line asynchronously waits 1 second before updating the values.
   // It's unnecessary here, but it simulates asynchronous behavior you often have to account for.
   await new Promise(resolve => setTimeout(resolve, 1000));
-
+  // @ts-ignore
   sun.value = sunval;
+  // @ts-ignore
   moon.value = moonval;
+  // @ts-ignore
   rising.value = risingval;
 }
